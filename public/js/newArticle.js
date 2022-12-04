34757b5ee0713d696cdc9bee03c56b62aff8b511
breakfireaus <myoungerc@hotmail.com>
2022-12-04 19:50:01 +1100

    fixed bugs

--- public/js/newArticle.js
+++ public/js/newArticle.js
@@ -1,42 +1,32 @@
-const addButtonHandler = async (event) => {
-    document.location.replace('dashboard/newArticle');
-  };
-  
-  const updateButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-      document.location.replace(`/dashboard/updateArticle/${id}`);
+const newArticleHandlerForm = async (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById('new-Article-Title').value.trim();
+  const content = document.getElementById('new-Article-Content').value.trim();
+
+  if (title && content) {
+    const response = await fetch('/api/articles/', {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
     } else {
-      alert('Update button did not have a data-id');
+      alert(response.statusText);
     }
-  };
-  
-  const deleteButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/articles/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to delete article');
-      }
-    } else {
-      alert('Delete button did not have a data-id');
-    }
-  };
-  
-  document
-    .querySelector('#btn-add')
-    .addEventListener('click', addButtonHandler);
-  
-  document
-    .querySelectorAll('.btn-update')
-    .forEach(btn => btn.addEventListener('click', updateButtonHandler));
-  
-  document
-    .querySelectorAll('.btn-delete')
-    .forEach(btn => btn.addEventListener('click', deleteButtonHandler));
+  }
+};
+
+const cancelButtonHandler = async () => {
+  document.location.replace('/dashboard');
+}
+
+document
+  .querySelector('.newArticleForm')
+  .addEventListener('submit', newArticleHandlerForm);
+
+document
+  .querySelector('.newArticleForm')
+  .addEventListener('reset', cancelButtonHandler);
