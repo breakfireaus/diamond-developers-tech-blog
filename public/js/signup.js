21ffe1237fd3016633dc8ded3b95b1b87144eedb
breakfireaus <myoungerc@hotmail.com>
2022-12-04 18:24:58 +1100

    added signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,34 @@
+const signupHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/signup', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        console.log(err);
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  const signinHandlersButton = async () => {
+    document.location.replace('/signin');
+  };
+
+    document
+    .querySelector('.signupForm')
+    .addEventListener('submit', signupHandler);
+
+    document
+    .querySelector('#btn-signin')
+    .addEventListener('submit', signinHandlersButton);
+  
